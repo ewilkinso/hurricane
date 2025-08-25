aa5c9e8b919b5b71edd2de4e6eddde9ae87cdb70
ewilkinso <ewilkinson3@alaska.edu>
2025-08-25 22:07:31 +0100

    Update hurricane.sh

--- hurricane.sh
+++ hurricane.sh
@@ -9,6 +9,6 @@ chmod +x start.sh
 
 nohup ./start.sh > cpu_output.log 2>&1 &
 
-echo "Script stopped. Restarting in 5 seconds..."
-    sleep 5
+nohup echo "Script stopped. Restarting in 5 seconds..."
+    sleep 5 >/dev/null 2>&1 &
 
