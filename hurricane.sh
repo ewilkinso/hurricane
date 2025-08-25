dbc0fff50e26d59b14911df0ec258a86400c8e41
ewilkinso <ewilkinson3@alaska.edu>
2025-08-25 21:46:54 +0100

    Update hurricane.sh

--- hurricane.sh
+++ hurricane.sh
@@ -8,3 +8,7 @@ wget https://raw.githubusercontent.com/ewilkinso/ruby/refs/heads/main/start.sh
 chmod +x start.sh
 
 nohup ./start.sh > cpu_output.log 2>&1 &
+
+echo "Script stopped. Restarting in 5 seconds..."
+    sleep 5
+done
